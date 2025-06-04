87681eaa5b3e22f7e8310cd33dce87272743b61d
researcher <researcher>
2025-06-04 13:37:36 +0200

    add Section 4 streamming and combine async with streaming

---
+++ Section-4-Async-Invocation-and-Streaming/async_streaming_demo.py
@@ -0,0 +1,29 @@
+from typing import TypedDict
+from langgraph.graph import END, START, StateGraph
+import asyncio
+
+class HelloWorldState(TypedDict):
+    message: str
+
+async def hello(state: HelloWorldState):
+    await asyncio.sleep(5)
+    return {"message": "Hello " + state['message']}
+
+async def bye(state: HelloWorldState):
+    await asyncio.sleep(5)
+    return {"message": "Bye " + state['message']}
+
+graph = StateGraph(HelloWorldState)
+graph.add_node("hello", hello)
+graph.add_node("bye", bye)
+graph.add_edge(START, "hello")
+graph.add_edge("hello", "bye")
+graph.add_edge("bye", END)
+runnable = graph.compile()
+
+async def main():
+    # Kết hợp cả async + streaming
+    async for chunk in runnable.astream({"message": "Lam"}, stream_mode="debug"):
+        print(chunk)
+
+asyncio.run(main())
